81094110d30f3afa7263d654c52eacbc470cc703
Suyash2116 <25130cn001@glbitm.ac.in>
2026-06-07 14:16:10 +0530

    Add files via upload

---
+++ Day 7/Recursive Factorial Q25.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Recursive_Factorial
+{
+    public static int fact(int n)
+    {
+        if(n == 1)
+        {
+            return 1;
+        }
+        return n * fact(n - 1);
+    }
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        int result = fact(n);
+        System.out.println("Recursive Factorial = " + result);
+    }
+}
