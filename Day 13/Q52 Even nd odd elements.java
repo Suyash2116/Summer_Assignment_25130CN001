742614eb7d7ed9fb2d2133d26ef596dda288ddb4
Suyash2116 <25130cn001@glbitm.ac.in>
2026-06-13 18:02:10 +0530

    Add files via upload

---
+++ Day 13/Q52 Even nd odd elements.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class EvenOddCount 
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        int evenCount = 0;
+        int oddCount = 0;
+        System.out.println("Enter array elements:");
+        for(int i = 0; i < n; i++) 
+        {
+            arr[i] = sc.nextInt();
+            if(arr[i] % 2 == 0) 
+            {
+                evenCount++;
+            }
+             else 
+            {
+                oddCount++;
+            }
+        }
+        System.out.println("Even elements = " + evenCount);
+        System.out.println("Odd elements = " + oddCount);
+    }
+}
